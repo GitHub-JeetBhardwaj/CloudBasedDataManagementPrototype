7e79d7ed55d106781bf6b3539728e8ea95b4f3ca
JEET BHARDWAJ <jeet90008@gmail.com>
2025-04-23 16:54:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -50,5 +50,7 @@ def delete_file(filename):
         os.remove(file_path)
     return redirect(url_for('index'))
 
-if __name__=='__main__':
-    app.run(debug=True)
+if __name__ == '__main__':
+    import os
+    port = int(os.getenv('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=os.getenv('FLASK_ENV') == 'development')
